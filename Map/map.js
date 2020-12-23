333e807ac3ed012f396ad9704c2ef334b62ef3f3
tech-doctor <55724699+tech-doctor@users.noreply.github.com>
2020-12-23 08:35:38 +0100

    updated commit on geosearch

--- Map/map.js
+++ Map/map.js
@@ -18,9 +18,6 @@
 
 
       function initAutocomplete() {
-        
-
-        
         var map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -33.8688, lng: 151.2195},
           zoom: 13,
